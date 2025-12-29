2d81fb18ed8adaca357a19de648a6cc744ebf004
Adrian Banaszak <69055180+xadrian11@users.noreply.github.com>
2025-12-29 16:49:40 +0100

    Add files via upload

---
+++ generated/vite.config.ts
@@ -0,0 +1,19 @@
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+});
